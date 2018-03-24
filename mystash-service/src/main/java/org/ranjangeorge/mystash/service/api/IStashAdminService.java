e17fdecfc9c05a3c61c06108a31581654765cf67
george-ranjan <george.ranjan@gmail.com>
2018-03-24 22:58:22 +0530

    Imporved name

--- mystash-service/src/main/java/org/ranjangeorge/mystash/service/api/IStashAdminService.java
+++ mystash-service/src/main/java/org/ranjangeorge/mystash/service/api/IStashAdminService.java
@@ -11,8 +11,8 @@ public interface IStashAdminService {
     @UsecaseName(Usecase.CREATE_NEW_STASH)
     String createNewStash(@NotNull String stashName);
 
-    @UsecaseName(Usecase.LIST_STASHES)
-    Set<String> list();
+    @UsecaseName(Usecase.LIST_ALL_STASHES)
+    Set<String> listAllStashes();
 
     @UsecaseName(Usecase.DELETE_ALL_STASHES)
     void deleteAllStashes();
