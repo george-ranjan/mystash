558e5e7c48c2b5ee6bb10583fe4215e14e7b759b
george-ranjan <george.ranjan@gmail.com>
2018-03-24 22:59:03 +0530

    Imporved name

--- mystash-service/src/main/java/org/ranjangeorge/mystash/service/api/support/Usecase.java
+++ mystash-service/src/main/java/org/ranjangeorge/mystash/service/api/support/Usecase.java
@@ -3,7 +3,7 @@ package org.ranjangeorge.mystash.service.api.support;
 public enum Usecase {
 
     CREATE_NEW_STASH,
-    LIST_STASHES,
+    LIST_ALL_STASHES,
     DELETE_ALL_STASHES,
 
     CREDIT,
